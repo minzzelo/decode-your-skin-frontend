80e1eb6d88b028d829e6310ece191ab470b845b4
Si Min Ong <ongsimin@Sis-MacBook-Air-2.local>
2020-05-19 19:31:34 +0800

    rebuilt nav bar

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -1,26 +1,57 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./styles.scss";
+import logo from "./logo.png";
+
+import { emphasize, withStyles } from "@material-ui/core/styles";
+import Breadcrumbs from "@material-ui/core/Breadcrumbs";
+import Chip from "@material-ui/core/Chip";
+import HomeIcon from "@material-ui/icons/Home";
+import PersonRoundedIcon from "@material-ui/icons/PersonRounded";
+import FavoriteRoundedIcon from "@material-ui/icons/FavoriteRounded";
+import BookIcon from "@material-ui/icons/Book";
+
+const StyledBreadcrumb = withStyles((theme) => ({
+  root: {
+    backgroundColor: "#ffffff",
+    color: theme.palette.grey[800],
+    fontWeight: theme.typography.fontWeightRegular,
+    "&:hover, &:focus": {
+      backgroundColor: theme.palette.grey[50],
+      cursor: "pointer",
+    },
+  },
+}))(Chip);
 
 export function Nav() {
-  const navStyle = {
-    color: "black",
-    textDecoration: "none",
-  };
-
   return (
-     <>
-      <nav>
-        <h3>Decode your skin</h3>
-        <ul className="nav-links">
-          <Link style={navStyle} to="/">
-            <li>Home</li>
-          </Link>
-          <Link style={navStyle} to="/login">
-            <li>Login</li>
-          </Link>
-        </ul>
-      </nav>
-    </>
+    <div style={{ marginBottom: 50 }}>
+      <img src={logo} />
+      <Breadcrumbs aria-label="breadcrumb">
+        <StyledBreadcrumb
+          component="a"
+          href="/"
+          label="Home"
+          icon={<HomeIcon fontSize="small" />}
+        />
+        <StyledBreadcrumb
+          component="a"
+          href="/"
+          label="Products"
+          icon={<FavoriteRoundedIcon fontSize="small" />}
+        />
+        <StyledBreadcrumb
+          component="a"
+          href="/"
+          label="My Skincare Diary"
+          icon={<BookIcon fontSize="small" />}
+        />
+        <StyledBreadcrumb
+          component="a"
+          href="/login"
+          label="Login / Register"
+          icon={<PersonRoundedIcon fontSize="small" />}
+        />
+      </Breadcrumbs>
+    </div>
   );
 }
